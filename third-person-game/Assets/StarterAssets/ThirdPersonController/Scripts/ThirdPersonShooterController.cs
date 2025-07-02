4eae91ebf6e31a7a440f056d6c914377139645b6
DusaPraharsh <dusapraharsh123@gmail.com>
2025-07-02 20:04:29 +0530

    character rotation fix while aiming

--- third-person-game/Assets/StarterAssets/ThirdPersonController/Scripts/ThirdPersonShooterController.cs
+++ third-person-game/Assets/StarterAssets/ThirdPersonController/Scripts/ThirdPersonShooterController.cs
@@ -1,6 +1,7 @@
 using Cinemachine;
 using UnityEngine;
 using StarterAssets;
+using UnityEngine.InputSystem;
 
 public class ThirdPersonShooterController : MonoBehaviour
 {
@@ -8,6 +9,8 @@ public class ThirdPersonShooterController : MonoBehaviour
     public GameObject crosshair;
     public float normalSensitivity;
     public float aimSensitivity;
+    public LayerMask aimColliderLayerMask = new LayerMask();
+    public Transform debugTransform;
 
     private ThirdPersonController thirdPersonController;
     private StarterAssetsInputs starterAssetsInputs;
@@ -19,17 +22,35 @@ public class ThirdPersonShooterController : MonoBehaviour
     }
     private void Update()
     {
+        Vector3 mouseWorldPosition = Vector3.zero;
+
+        Vector2 screenCenterPoint = new Vector2(Screen.width / 2f, Screen.height / 2f);
+        Ray ray = Camera.main.ScreenPointToRay(screenCenterPoint);
+        if (Physics.Raycast(ray, out RaycastHit raycastHit, 999f, aimColliderLayerMask))
+        {
+            debugTransform.position = raycastHit.point;
+            mouseWorldPosition = raycastHit.point;
+        }
+
         if (starterAssetsInputs.aim)
         {
             aimVirCam.gameObject.SetActive(true);
             thirdPersonController.SetSensitivity(aimSensitivity);
             crosshair.SetActive(true);
+            thirdPersonController.SetRotationOnMove(false);
+
+            Vector3 worldAimTarget = mouseWorldPosition;
+            worldAimTarget.y = transform.position.y;
+            Vector3 aimDirection = (worldAimTarget - transform.position).normalized;
+
+            transform.forward = Vector3.Lerp(transform.forward, aimDirection, Time.deltaTime * 20f);
         }
         else
         {
             aimVirCam.gameObject.SetActive(false);
             thirdPersonController.SetSensitivity(normalSensitivity);
             crosshair.SetActive(false);
+            thirdPersonController.SetRotationOnMove(true);
         }
     }
 }
